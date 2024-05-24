1d97354fc1b0df41b43057777c65ac31a438bdc4
Ideilson Souza <ideilson@icware.com.br>
2024-05-24 19:42:52 -0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     // 'allowed_methods' => ['GET', 'POST','PUT', 'OPTIONS', 'PATCH'],
 
-    'allowed_origins' => ['http://pathsdk.com.br'],
+    'allowed_origins' => ['zaimineracao.com.br','web.zaimineracao.com.br'],
 
     // 'allowed_origins_patterns' => [],
 
